e04b791e08ee934564dd0ebf6bef759bf689dc79
Wahyu-Sedana <wsstudiocode@gmail.com>
2025-04-21 10:28:02 +0800

    fix: add env.d.ts

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -51,7 +51,7 @@
             <iframe
               class="w-full h-full"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63397.01394064742!2d115.17802415826262!3d-8.67216805501193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd24704e9a5fdfb%3A0x4030bfbca7d1090!2sDenpasar%2C%20Bali!5e0!3m2!1sen!2sid!4v1713742762831!5m2!1sen!2sid"
-              allowfullscreen=""
+              allowfullscreen
               loading="lazy"
               referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
